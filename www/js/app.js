7e99f82aad57a28439050047c4b5467403fce7b1
rogermelich <rogermelich@gmail.com>
2016-01-25 09:50:55 +0100

    Add todos reoutes to todos.html

--- www/js/app.js
+++ www/js/app.js
@@ -121,6 +121,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'io
             }
         }
     })
+    
+
+    .state('app.todos', {
+        url: '/todos',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/todos.html',
+                controller: 'TodosCtrl'
+            },
+            'fabContent': {
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                controller: function ($timeout) {
+                    /*$timeout(function () {
+                        document.getElementById('fab-profile').classList.toggle('on');
+                    }, 800);*/
+                }
+            }
+        }
+    })
     ;
 
     // if none of the above states are matched, use this as the fallback
